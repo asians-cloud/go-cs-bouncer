9972840d0d36e49c62c78f931a411129d9faf432
Komang Suryadana <suryadana80@gmail.com>
2023-09-14 10:42:21 +0800

    Implement SSE

--- stream_bouncer.go
+++ stream_bouncer.go
@@ -230,7 +230,6 @@ func (b *StreamBouncer) RunStream(ctx context.Context) {
 
 			// Decode each JSON object
 			if err := decoder.Decode(data); err != nil {
-				log.Error("Error decoding JSON:", err)
 				continue
 			}
 
